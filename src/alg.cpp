109accc5c87314e6e0e8e9590cd642eefcae9517
blackberry12562 <116704068+blackberry12562@users.noreply.github.com>
2023-03-14 17:04:29 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,28 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int left = 0;
+  int right = size - 1;
+  int kolv = 0;
+  while (right >= left) {
+    int mid = (right + left) / 2;
+    if (*(arr + mid) == value) {
+      kolv += 1;
+      int newmid = mid;
+      while ((*(arr + mid + 1)) == value) {
+        kolv += 1;
+        mid += 1;
+      }
+      while ((*(arr + newmid - 1)) == value) {
+        kolv += 1;
+        newmid -= 1;
+      }
+      return kolv;
+    } else if (*(arr + mid) > value) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return kolv;
 }
